07d0dc0cf87ff304a2f68e22acd6ed62d54825d5
phTrung <56182175+phTrung@users.noreply.github.com>
2022-07-05 21:33:51 -0400

    comments imageprocessing

--- LAB02/imageProcessing.py
+++ LAB02/imageProcessing.py
@@ -1,22 +1,9 @@
 import os
-
 from pathlib import Path
-
 import matplotlib.pyplot as plt
-
 import cv2
-
 import numpy as np
 import imutils
-import random
-
-import pickle
-
-import ImageClassifier
-
-from tabulate import tabulate
-
-from squares import find_squares
 
 WIDTH = 100
 
@@ -25,6 +12,7 @@ HEIGHT = 100
 
 def formatFolderPic(folder):
     x = []
+    #itération sur les images du fichier de l'ensemble B
     for path in os.listdir(folder):
         picPath = str(folder) + "/" + str(path)
         x.append(formatPic(picPath))
@@ -32,19 +20,27 @@ def formatFolderPic(folder):
     print(x)
     return x
 
-
+#methode pour le prétraitement des images
 def formatPic(path):
+    #cherche l'image
     image = cv2.imread(path)
+    #inverse la valeur des pixels
     bitwise = cv2.bitwise_not(image)
+    #redimentionne l'image en 100 par 100
     resized = cv2.resize(bitwise, (WIDTH, HEIGHT))
+    #convertie l'image en gradient de gris
     gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    #L’opération de noyau gaussien est utilisée pour flouter un peu l’image.
+    # Le flou gaussien est très efficace pour supprimer le bruit gaussien d'une image.
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    #Extremise la valeur des pixels pour avoir que du noir ou bland (0 ou 255)
     thresh = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY)[1]
 
+    #convertie l'image en array numpy
     imagearray = np.array(thresh)
-    #cv2.imshow('image', imagearray)
-    #cv2.waitKey(0)
+    #change les pixels de l'image pour avoir que des valeurs binaires
     imagearray[imagearray == 255] = 1
+    #applatie la matrice pour avoir un vecteur
     return imagearray.reshape(1, 10000)
 
 
