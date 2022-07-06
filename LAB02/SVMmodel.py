6bd940258be7a4ac4e553227d442b0df8ed2c1ab
phTrung <56182175+phTrung@users.noreply.github.com>
2022-07-05 21:18:44 -0400

    commentaire SVM

--- LAB02/SVMmodel.py
+++ LAB02/SVMmodel.py
@@ -7,9 +7,14 @@ from sklearn import metrics
 
 def SVM(X,Y):
 
+    #Fait la séparation des données pour avoir les données X et Y de test et d'entrainement
     Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2, random_state=59)
+
+    #Création du classificateur
     clf = SVC(kernel='linear')
     clf.fit(Xtrain, Ytrain)
+
+    #La prédiction
     Ypred = clf.predict(Xtest)
 
     return Xtrain, Xtest, Ytrain, Ytest, Ypred
@@ -17,16 +22,19 @@ def SVM(X,Y):
 
 def main():
 
+
     folder = "EnsembleB"
     #dataset
     X = imageProcessing.formatFolderPic(folder)
     Y = []
 
+    #Creation de notre matrice ayant le même nombre de données que le nombre d'image dans l'ensemble B
     for i, direct in enumerate(folder):
         Y.append(i)
 
     Xtrain, Xtest, Ytrain, Ytest, Ypred = SVM(X,Y)
 
+    #Génération des résultats grâce au données de SVM
     print("Accuracy:", metrics.accuracy_score(Ytest, Ypred))
     print("Precision:", metrics.precision_score(Ytest, Ypred))
     print("Recall:", metrics.recall_score(Ytest, Ypred))
